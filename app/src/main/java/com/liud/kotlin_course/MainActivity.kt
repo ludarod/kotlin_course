8dd2d70b544fd34a0fe3500cbe0df7f53c6c7c2a
lcrod <44011097+lcrod@users.noreply.github.com>
2024-03-17 21:52:03 -0400

    Initial course, Main function definition

---
+++ app/src/main/java/com/liud/kotlin_course/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.liud.kotlin_course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Main()
+    }
+}
+
+fun Main(){
+    println("Bienvenido")
+}
