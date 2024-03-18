5d4f7efd17ddfbb41354c9b0b79a6184d0f8cfd1
lcrod <44011097+lcrod@users.noreply.github.com>
2024-03-17 22:59:46 -0400

    Convertions and concat in Kotlin

--- app/src/main/java/com/liud/kotlin_course/Convertions.kt
+++ app/src/main/java/com/liud/kotlin_course/Convertions.kt
@@ -9,4 +9,11 @@ fun Convertios(){
     var chainString:String = numberInt.toString()
     println(chainString)
 
+    // Concat variables or const with type String variable
+    print("This is a numeric var $numberInt, and this is a numeric var covert to 'String' type $chainString")
+
+    // Another way to concat string
+
+    println(chainString + "this is concat also")
+
 }
